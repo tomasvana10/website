c5297b5beabab3faa3f46778ab406fddcb6daa60
Tomas Vana <tomasvana@yahoo.com>
2024-09-09 11:53:07 +1000

    commit code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import daisy from "daisyui"
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [daisy],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
 };
 export default config;
