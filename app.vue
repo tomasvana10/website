bd1072758cc3d6ba79de81625fe7796b0fe60a72
tomasvana10 <tomasvana@yahoo.com>
2025-07-01 20:20:15 +1000

    add theme switcher

--- app.vue
+++ app.vue
@@ -1,15 +1,8 @@
-<script setup>
-import { useDark } from '@vueuse/core';
-
-onMounted(() => {
-  useDark();
-})
-</script>
-
 <template>
   <NuxtLayout>
     <h1 class="dark:text-5xl">hello</h1>
     <p>Hello</p>
+    <ThemeSwitcher />
   </NuxtLayout>
 </template>
 
---
+++ components/ThemeSwitcher.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { UseDark } from "@vueuse/components";
+</script>
+
+<template>
+  <ClientOnly>
+    <UseDark v-slot="{ isDark, toggleDark }">
+      <button @click="toggleDark()">Is Dark: {{ isDark }}</button>
+    </UseDark>
+  </ClientOnly>
+</template>
